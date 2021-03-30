e3526d078adc8c0a656f16aef6546ae052ae37b9
pavlin.marinov4@gmail.com <pavlin.marinov4@gmail.com>
2021-03-30 20:51:52 +0300

    Add room service

---
+++ src/Services/IRoomService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Data.Models;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Enums;
+
+namespace Services
+{
+    public interface IRoomService
+    {
+        public Task AddRoom(Room room);
+
+        public Task<IEnumerable<T>> GetAllByCapacity<T>(int capacity) where T : class;
+
+        public Task<IEnumerable<T>> GetAllByType<T>(RoomType type) where T : class;
+
+        public Task<IEnumerable<T>> GetAllReservedRooms<T>() where T : class;
+
+        public Task DeleteRoom(string id);
+
+        public Task UpdateRoom(Room room);
+
+        public Task<T> GetRoom<T>(string id) where T : class;
+       
+    }
+  
+}
