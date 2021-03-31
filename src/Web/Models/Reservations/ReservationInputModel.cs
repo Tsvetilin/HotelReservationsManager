d11a30e6c4e18acfbca587486c79f1b2721522ed
Цветилин Цветилов <cvetilov6@abv.bg>
2021-03-31 22:39:47 +0300

    Disable property bindings

--- src/Web/Models/Reservations/ReservationInputModel.cs
+++ src/Web/Models/Reservations/ReservationInputModel.cs
@@ -1,4 +1,5 @@
 ﻿using Data.Enums;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using Web.Models.Clients;
@@ -16,15 +17,23 @@ namespace Web.Models.Reservations
         public bool Breakfast { get; set; }
         public bool AllInclusive { get; set; }
         //should be passed down
+        [BindNever]
         public double BreakfastPrice { get; set; }
+        [BindNever]
         public double AllInclusivePrice { get; set; }
-        //should be verified on submit
+        [BindNever]
         public double Price { get; set; }
+        [BindNever]
         public string RoomId { get; set; }
+        [BindNever]
         public IEnumerable<ReservationPeriod> Reservations { get; set; }
+        [BindNever]
         public int RoomCapacity { get; set; }
+        [BindNever]
         public RoomType RoomType { get; set; }
+        [BindNever]
         public double RoomAdultPrice { get; set; }
+        [BindNever]
         public double RoomChildrenPrice { get; set; }
     }
 }
