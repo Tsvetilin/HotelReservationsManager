567cb88eb335950cf1a5820fbfba6e6f73601e35
Tony5768 <tonkatazl@gmail.com>
2021-04-05 12:37:25 +0300

    Add NUnit tests

---
+++ src/Tests/Service.Tests/UserServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Services;
+using Services.External;
+
+namespace Tests.Service.Tests
+{
+    class UserServiceTests
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
